37c83b4aef468c864f01cf665f14f363e5baec4b
OLAMIDE <lutholamide78@gmail.com>
2026-06-11 15:08:02 +0100

    fpush

---
+++ flappybirds.js
@@ -0,0 +1,54 @@
+
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34; // width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+let birdImg;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
+// pipes 
+let pipeArray = [];
+let pipeWidth = 64; //width/height ratio =384/3072 =1/8
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board 
+
+    // let draw flappy birds 
+    //context.fillStyle = "green";
+    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    //load images
+    birdImg = new Image();
+    birdImg.src = "./flappybird.png";
+    birdImg.onload = function() {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    requestAnimationFrame(update);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    //bird 
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+}
